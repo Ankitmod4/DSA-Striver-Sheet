fa9eb02f5ff3726c23abf9099a65b90f62e2e756
Ankit Mod <148241800+Ankitmod4@users.noreply.github.com>
2025-01-20 23:09:52 +0530

    Update 06_Maximum_SubSequence_Sum.cpp

--- 25_DP/06_Maximum_SubSequence_Sum.cpp
+++ 25_DP/06_Maximum_SubSequence_Sum.cpp
@@ -10,29 +10,29 @@ using namespace std;
 class Solution{
 
         //Tabulation DP
-	public:
-	int maxSumIS(int arr[], int n)  
-	{  
-	    int dp[n];
-	    
-	    for(int i=0;i<n;i++){
-	        dp[i]=arr[i];
-	    }
-	    
-	    for(int i=1;i<n;i++){
-	        for(int j=0;j<i;j++){
-	            if(arr[j]<arr[i]){
-	                if(dp[i]< arr[i]+dp[j]){
-	                    dp[i]=arr[i]+dp[j];
-	                }
-	            }
-	        }
-	    }
-	    
-	    int maxi=INT_MIN;
-	    for(int i=0;i<n;i++){
-	        maxi=max(maxi,dp[i]);
-	    }
+	int maxSumIncreasingSubsequence(vector<int>& a) {
+    int n = a.size();
+    vector<int> dp(n);
+    
+    for (int i = 0; i < n; i++) {
+        dp[i] = a[i];
+    }
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < i; j++) {
+            if (a[j] < a[i]) {
+                dp[i] = max(dp[i], dp[j] + a[i]);
+            }
+        }
+    }
+    
+    int ma = 0;
+    for (int i = 0; i < n; i++) {
+        ma = max(ma, dp[i]);
+    }
+    
+    return ma;
+}
 	    
 	    return maxi;
 	}  
